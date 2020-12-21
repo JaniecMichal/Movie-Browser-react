18ab30ca160657fd4f6b031c96c0bf315a715d7e
Michal Janiec <michal.janiec95@gmail.com>
2020-12-21 22:45:53 +0100

    Updates

---
+++ precache-manifest.f5fd3ecfedad2300239f564ba4383dca.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd3b17dec095d542e294d53c2f57b9e",
+    "url": "/JaniecMichal/Movie-Browser-react/index.html"
+  },
+  {
+    "revision": "918c7eed5d9f07082578",
+    "url": "/JaniecMichal/Movie-Browser-react/static/js/2.4a540707.chunk.js"
+  },
+  {
+    "revision": "0bfb10e5d591fad7dc1e73daaaf7af7e",
+    "url": "/JaniecMichal/Movie-Browser-react/static/js/2.4a540707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e992d0448c7a390aaec",
+    "url": "/JaniecMichal/Movie-Browser-react/static/js/main.0ab83817.chunk.js"
+  },
+  {
+    "revision": "6a4d6202dff838f53bd9",
+    "url": "/JaniecMichal/Movie-Browser-react/static/js/runtime-main.4de6490c.js"
+  },
+  {
+    "revision": "8815fed74789d799a41cc4b6cb72e2a0",
+    "url": "/JaniecMichal/Movie-Browser-react/static/media/Danger.8815fed7.svg"
+  },
+  {
+    "revision": "b6c0d62525d1916a1af62f3827c15425",
+    "url": "/JaniecMichal/Movie-Browser-react/static/media/NoPoster.b6c0d625.svg"
+  },
+  {
+    "revision": "3cb4c491c23a2cca42d8413c242ac240",
+    "url": "/JaniecMichal/Movie-Browser-react/static/media/Picture.3cb4c491.svg"
+  },
+  {
+    "revision": "56dd92fe52ec8fc3a08c48956b883f60",
+    "url": "/JaniecMichal/Movie-Browser-react/static/media/Search.56dd92fe.svg"
+  },
+  {
+    "revision": "cdf24f103dd27027831a744b87c4311d",
+    "url": "/JaniecMichal/Movie-Browser-react/static/media/Video.cdf24f10.svg"
+  },
+  {
+    "revision": "482a96f7e82072146b3b68bb0efddfe1",
+    "url": "/JaniecMichal/Movie-Browser-react/static/media/film.482a96f7.svg"
+  },
+  {
+    "revision": "d057d55a6dfeb6dda7520b7d0b0b632c",
+    "url": "/JaniecMichal/Movie-Browser-react/static/media/spinner.d057d55a.svg"
+  },
+  {
+    "revision": "b489e61393b0ed1bb8e71ee82f0e4821",
+    "url": "/JaniecMichal/Movie-Browser-react/static/media/star.b489e613.svg"
+  }
+]);
