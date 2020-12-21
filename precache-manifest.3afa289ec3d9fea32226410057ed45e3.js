e75c115d47ec5b91bb9ff927d3ce36207dc8871a
Michal Janiec <michal.janiec95@gmail.com>
2020-12-21 23:15:35 +0100

    Updates

---
+++ precache-manifest.3afa289ec3d9fea32226410057ed45e3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2dcba2e2aefd24379d0e59d87d7dec8",
+    "url": "/Movie-Browser-react/index.html"
+  },
+  {
+    "revision": "54ed8643af4ad7e31b79",
+    "url": "/Movie-Browser-react/static/js/2.67d46305.chunk.js"
+  },
+  {
+    "revision": "0bfb10e5d591fad7dc1e73daaaf7af7e",
+    "url": "/Movie-Browser-react/static/js/2.67d46305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e992d0448c7a390aaec",
+    "url": "/Movie-Browser-react/static/js/main.0ab83817.chunk.js"
+  },
+  {
+    "revision": "6e4e63d5a1567c390883",
+    "url": "/Movie-Browser-react/static/js/runtime-main.e7f427bb.js"
+  },
+  {
+    "revision": "8815fed74789d799a41cc4b6cb72e2a0",
+    "url": "/Movie-Browser-react/static/media/Danger.8815fed7.svg"
+  },
+  {
+    "revision": "b6c0d62525d1916a1af62f3827c15425",
+    "url": "/Movie-Browser-react/static/media/NoPoster.b6c0d625.svg"
+  },
+  {
+    "revision": "3cb4c491c23a2cca42d8413c242ac240",
+    "url": "/Movie-Browser-react/static/media/Picture.3cb4c491.svg"
+  },
+  {
+    "revision": "56dd92fe52ec8fc3a08c48956b883f60",
+    "url": "/Movie-Browser-react/static/media/Search.56dd92fe.svg"
+  },
+  {
+    "revision": "cdf24f103dd27027831a744b87c4311d",
+    "url": "/Movie-Browser-react/static/media/Video.cdf24f10.svg"
+  },
+  {
+    "revision": "482a96f7e82072146b3b68bb0efddfe1",
+    "url": "/Movie-Browser-react/static/media/film.482a96f7.svg"
+  },
+  {
+    "revision": "d057d55a6dfeb6dda7520b7d0b0b632c",
+    "url": "/Movie-Browser-react/static/media/spinner.d057d55a.svg"
+  },
+  {
+    "revision": "b489e61393b0ed1bb8e71ee82f0e4821",
+    "url": "/Movie-Browser-react/static/media/star.b489e613.svg"
+  }
+]);
